56680fd4fc714403c24e00dabceda557ddb847fa
invpe <106522950+invpe@users.noreply.github.com>
2025-01-21 13:26:19 +0100

    Update autostart.sh

--- Release/Scripts/autostart.sh
+++ Release/Scripts/autostart.sh
@@ -24,17 +24,17 @@ sleep 60
 # Detect architecture
 ARCH=$(uname -m)
 if [[ "$ARCH" == "x86_64" ]]; then
-    BINARY_URL="https://github.com/invpe/GRIDMAN/releases/latest/download/gmworker_linux64.bin"
-    BINARY_NAME="gmworker_linux64.bin"
+    BINARY_URL="https://github.com/invpe/GRIDMAN/releases/latest/download/gmworker_linux64"
+    BINARY_NAME="gmworker_linux64"
 elif [[ "$ARCH" == "i686" || "$ARCH" == "i386" ]]; then
-    BINARY_URL="https://github.com/invpe/GRIDMAN/releases/latest/download/gmworker_linux32.bin"
-    BINARY_NAME="gmworker_linux32.bin"
+    BINARY_URL="https://github.com/invpe/GRIDMAN/releases/latest/download/gmworker_linux32"
+    BINARY_NAME="gmworker_linux32"
 elif [[ "$ARCH" == "aarch64" ]]; then
-    BINARY_URL="https://github.com/invpe/GRIDMAN/releases/latest/download/gmworker_arm64.bin"
-    BINARY_NAME="gmworker_arm64.bin"
+    BINARY_URL="https://github.com/invpe/GRIDMAN/releases/latest/download/gmworker_arm64"
+    BINARY_NAME="gmworker_arm64"
 elif [[ "$ARCH" == "armv7l" || "$ARCH" == "armhf" ]]; then
-    BINARY_URL="https://github.com/invpe/GRIDMAN/releases/latest/download/gmworker_arm32.bin"
-    BINARY_NAME="gmworker_arm32.bin"
+    BINARY_URL="https://github.com/invpe/GRIDMAN/releases/latest/download/gmworker_arm32"
+    BINARY_NAME="gmworker_arm32"
 else
     echo "Unsupported architecture: $ARCH"
     exit 1
